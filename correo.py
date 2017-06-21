d90db527b2b6ba253f8a4bc326f630ab1c0b7ade
Jairo Gonzalez <jairo.glez@hotmail.com>
2017-06-20 21:25:25 -0500

    convertido a clase

--- correo.py
+++ correo.py
@@ -1,19 +1,20 @@
 import sqlite3
 
-db = sqlite3.connect("correo.db")
-c = db.cursor()
+class Correo:
+    db = sqlite3.connect("correo.db")
+    c = db.cursor()
 
-def mostrarCuenta():
-    r = c.execute("SELECT * FROM cuentaCorreo")
-    r = r.fetchall()
+    def mostrarCuenta(self):
+        r = self.c.execute("SELECT * FROM cuentaCorreo")
+        r = r.fetchall()
+        lista = []
 
-    for fila in r:
-        print(fila)
+        for fila in r:
+            lista.append(fila)
 
-def insertarCuenta(datos):
-    c.execute("INSERT INTO  cuentaCorreo (nombre, direccion, contraseña) VALUES (?,?,?)",
-    (datos[0],datos[1],datos[2]))
-    db.commit()
+        return lista
 
-insertarCuenta(["luis","luis877@gmail.com","contraseña"])
-mostrarCuenta()
+    def insertarCuenta(self,datos):
+        self.c.execute("INSERT INTO  cuentaCorreo (nombre, direccion, contraseña) VALUES (?,?,?)",
+        (datos[0],datos[1],datos[2]))
+        self.db.commit()
